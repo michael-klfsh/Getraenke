91c4f36d6926f6658ccd0016a53e574811fdb59a
Michael Kleefisch <michael.kleefisch@rwth-aachen.de>
2023-02-27 14:45:18 +0100

    Add new project

---
+++ getraenke/src/main.js
@@ -0,0 +1,10 @@
+import Vue, { createApp } from 'vue'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+createApp(App).mount('#app')
