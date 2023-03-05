e26cd6e4b6a75e92035a37734657074cd520a898
Michael Kleefisch <michael.kleefisch@rwth-aachen.de>
2023-03-05 17:52:24 +0100

    Add some queries for user

--- getraenke_backend/app/models/user.model.js
+++ getraenke_backend/app/models/user.model.js
@@ -1,28 +1,97 @@
-//const { sequelize, Sequelize } = require(".");  //wurde automatisch hinzugefügt
+const sql = require("./db.js");
 
-module.exports = (sequelize, Sequelize) => {
-    const User = sequelize.define("user", {
-        vorname: {
-            type: Sequelize.STRING
-        },
-        nachname: {
-            type: Sequelize.STRING
-        },
-        email: {
-            type: Sequelize.STRING
-        },
-        isFAHO: {
-            type: Sequelize.BOOLEAN
-        },
-        splitwise: {
-            type: Sequelize.BOOLEAN
-        },
-        zimmerNr: {
-            type: Sequelize.INTEGER
-        }
-    }, {
-        timestamps: false
+
+// constructor
+const User = function(user) {
+    this.vorname = user.title;
+    this.nachname = user.nachname;
+    this.isFaho = user.isFaho;
+    this.zimmerNr = user.zimmerNr;
+    this.splitwise = user.splitwise;
+    this.email = user.email;
+  };
+  
+  User.create = (newUser, result) => {
+    sql.query("INSERT INTO User SET ?", newUser, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+  
+      console.log("created User: ", { id: res.insertId, ...newUser });
+      result(null, { id: res.insertId, ...newUser });
     });
-
-    return User;
-};
+  };
+  
+  User.findById = (id, result) => {
+    sql.query(`SELECT * FROM User WHERE id = ${id}`, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+  
+      if (res.length) {
+        console.log("found user: ", res[0]);
+        result(null, res[0]);
+        return;
+      }
+  
+      // not found User with the id
+      result({ kind: "not_found" }, null);
+    });
+  };
+  
+  User.getAll = (title, result) => {
+    let query = "SELECT * FROM User";
+  
+    if (title) {
+      query += ` WHERE title LIKE '%${title}%'`;
+    }
+  
+    sql.query(query, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+  
+      console.log("users: ", res);
+      result(null, res);
+    });
+  };
+  
+  User.remove = (id, result) => {
+    sql.query("DELETE FROM User WHERE id = ?", id, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+  
+      if (res.affectedRows == 0) {
+        // not found User with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+  
+      console.log("deleted user with id: ", id);
+      result(null, res);
+    });
+  };
+  
+  User.removeAll = result => {
+    sql.query("DELETE FROM User", (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+  
+      console.log(`deleted ${res.affectedRows} users`);
+      result(null, res);
+    });
+  };
+  
+  module.exports = User;
