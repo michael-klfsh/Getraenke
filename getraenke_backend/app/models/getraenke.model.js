9d1b5242a1f6bb4a76434b5be38c066294a0a207
Michael Kleefisch <michael.kleefisch@rwth-aachen.de>
2023-03-05 18:26:31 +0100

    Rework getraenke model and controller

--- getraenke_backend/app/models/getraenke.model.js
+++ getraenke_backend/app/models/getraenke.model.js
@@ -1,14 +1,77 @@
-module.exports = (sequelize, Sequelize) => {
-    const Getraenk = sequelize.define("getraenk", {
-        name: {
-            type: Sequelize.STRING
-        },
-        preis: {
-            type: Sequelize.DOUBLE
-        }
-    }, {
-        timestamps: false
-    });
+const sql = require("./db.js");
 
-    return Getraenk;
+const Getraenk = function(getraenk) {
+    this.name = getraenk.name;
+    this.preis = getraenk.preis;
 };
+
+Getraenk.create = (newGetraenk, result) => {
+    sql.query("INSERT INTO Getraenke SET ?", newGetraenk, (err, res) => {
+        if(err) {
+            result(err, null);
+            return;
+        }
+
+        result(null, { id: res.insertId, ...newGetraenk });
+    });
+};
+
+Getraenk.findById = (id, result) => {
+    sql.query(`SELECT * FROM Getraenke WHERE id = ${id}`, (err, res) => {
+      if (err) {
+        result(err, null);
+        return;
+      }
+  
+      if (res.length) {
+        result(null, res[0]);
+        return;
+      }
+  
+      // not found User with the id
+      result({ kind: "not_found" }, null);
+    });
+};
+
+User.getAll = (result) => {
+    let query = "SELECT * FROM Getraenke";
+  
+    sql.query(query, (err, res) => {
+      if (err) {
+        result(null, err);
+        return;
+      }
+
+      result(null, res);
+    });
+};
+
+Getraenk.remove = (id, result) => {
+    sql.query("DELETE FROM Getraenke WHERE id = ?", id, (err, res) => {
+      if (err) {
+        result(null, err);
+        return;
+      }
+  
+      if (res.affectedRows == 0) {
+        // not found User with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      result(null, res);
+    });
+};
+  
+  Getraenk.removeAll = result => {
+    sql.query("DELETE FROM Getraenke", (err, res) => {
+      if (err) {
+        result(null, err);
+        return;
+      }
+
+      result(null, res);
+    });
+};
+  
+module.exports = Getraenk;
