6b5de1e5e24ec1b172140ff8be4d57f22fed1430
Michael Kleefisch <michael.kleefisch@rwth-aachen.de>
2023-03-05 12:50:52 +0100

    Add user model with sequelize ORM

---
+++ getraenke_backend/app/models/user.model.js
@@ -0,0 +1,26 @@
+//const { sequelize, Sequelize } = require(".");  //wurde automatisch hinzugefügt
+
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("user", {
+        vorname: {
+            type: Sequelize.STRING
+        },
+        nachname: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        isFAHO: {
+            type: Sequelize.BOOLEAN
+        },
+        splitwise: {
+            type: Sequelize.BOOLEAN
+        },
+        zimmerNr: {
+            type: Sequelize.INT
+        }
+    });
+
+    return User;
+};
