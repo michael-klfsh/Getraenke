c1931561ec9e924829fca14b8dc983964b3e6e7f
Michael Kleefisch <michael.kleefisch@rwth-aachen.de>
2023-03-05 13:35:01 +0100

    Add note

--- getraenke_backend/app/config/db.config.js
+++ getraenke_backend/app/config/db.config.js
@@ -1,7 +1,7 @@
 module.export = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "123456",
+    PASSWORD: "123456",     //Passwort in Env auslagern oder die Datei nur als Template beibehalten!
     DB: "testdb",
     dialect: "mariadb",
     pool: {
