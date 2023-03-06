205719888fa6772ccf266d040903c47598bc640b
Michael Kleefisch <michael.kleefisch@rwth-aachen.de>
2023-03-06 15:33:26 +0100

    Add db query for kauf

--- getraenke_backend/app/models/kauf.model.js
+++ getraenke_backend/app/models/kauf.model.js
@@ -1,11 +1,68 @@
-module.exports = (sequelize, Sequelize) => {
-    const Kauf = sequelize.define("kauf", {
-        anzahl: {
-            type: Sequelize.INTEGER
-        }
-    }, {
-        timestamps: true
-    });
+const sql = require("./db.js");
 
-    return Kauf;
+const Kauf = function(kauf) {
+    this.getraenkeId = kauf.getraenkeId;
+    this.userId = kauf.userId;
+    this.anzahl = kauf.anzahl;
+};
+
+
+
+Kauf.create = (newKauf, result) => {
+    sql.query("INSERT INTO UserKauftGetraenk SET ?", newKauf, (err, res) => {
+        if(err) {
+            result(err, null);
+            return;
+        }
+        result(null, {timestamp: res.timestamp, ...newKauf});
+    });
+}
+
+Kauf.findById = (userId, getraenkId, timestamp, result) => {
+    sql.query(`SELECT * FROM UserKauftGetraenk WHERE userId = ${userId} AND getraenkId = ${getraenkId} AND timestamp >= ${timestamp}`, (err, res) => {
+        if(err) {
+            result(err, null);
+            return;
+        }
+
+        if(res.length) {
+            result(null, res[0]);
+            return;
+        }
+
+        result({kind: "not_found"}, null);
+    });
+};
+
+Kauf.getAllByUser = (userId, result) => {
+    sql.query(`SELECT * FROM UserKauftGetraenk WHERE userId = ${userId}`, (err, res) => {
+        if(err) {
+            result(null, err);
+            return;
+        }
+
+        result(null, res);
+    });
+};
+
+Kauf.getFromUserTimestamp = (userId, timestamp, result) => {
+    sql.query(`SELECT * FROM UserKauftGetraenk WHERE userID = ${userId} AND timestamp >= ${timestamp}`, (err, res) => {
+        if(err) {
+            result(null, err);
+            return;
+        }
+
+        result(null, res);
+    });
+};
+
+Kauf.getAfter = (timestamp, result) => {
+    sql.query(`SELECT * FROM UserKauftGetraenk WHERE timestamp >= ${timestamp}`, (err, res) => {
+        if(err) {
+            result(null, err);
+            return;
+        }
+
+        result(null, res);
+    });
 };
