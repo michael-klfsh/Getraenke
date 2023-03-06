6832f9d44b89a0f8ddb5f0715dbcf2e06dc09fd6
Michael Kleefisch <michael.kleefisch@rwth-aachen.de>
2023-03-06 20:31:45 +0100

    Rework user model to query data from db

--- getraenke_backend/app/models/user.model.js
+++ getraenke_backend/app/models/user.model.js
@@ -4,90 +4,91 @@ const sql = require("./db.js");
 
 // constructor
 const User = function(user) {
-    this.vorname = user.title;
-    this.nachname = user.nachname;
-    this.isFaho = user.isFaho;
-    this.zimmerNr = user.zimmerNr;
-    this.splitwise = user.splitwise;
-    this.email = user.email;
-  };
+  this.vorname = user.title;
+  this.nachname = user.nachname;
+  this.isFaho = user.isFaho;
+  this.zimmerNr = user.zimmerNr;
+  this.splitwise = user.splitwise;
+  this.email = user.email;
+};
   
-  User.create = (newUser, result) => {
-    sql.query("INSERT INTO Nutzer SET ?", newUser, (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
+User.create = (newUser, result) => {
+  sql.then(connection => {
+    connection.query("INSERT INTO Nutzer SET ?", newUser)
+    .then(rows => {
+      console.log("created User: ", { id: rows.insertId, ...newUser });
+      result(null, { id: rows.insertId, ...newUser });
+    })
+    .catch(err => {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    })
+  });
+};
+  
+User.findById = (id, result) => {
+  sql.then(connection => {
+    connection.query(`SELECT * FROM Nutzer WHERE id = ${id}`)
+    .then(rows => {
+      if (rows.length) {
+        console.log("found user: ", rows[0]);
+        result(null, rows[0]);
         return;
       }
-  
-      console.log("created User: ", { id: res.insertId, ...newUser });
-      result(null, { id: res.insertId, ...newUser });
+      console.log("not found");
+      result({ kind: "not_found" }, null);
+    })
+    .catch(err => {
+      console.log("error: ", err);
+      result(err, null);
     });
-  };
+  });
+};
   
-  User.findById = (id, result) => {
-    sql.then(connection => {
-      connection.query(`SELECT * FROM Nutzer WHERE id = ${id}`)
-      .then(rows => {
-        if (rows.length) {
-          console.log("found user: ", rows[0]);
-          result(null, rows[0]);
-          return;
-        }
-        console.log("not found");
-        result({ kind: "not_found" }, null);
-      })
-      .catch(err => {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      });
+User.getAll = (result) => {
+  sql.then(connection => {
+    connection.query("SELECT * FROM Nutzer")
+    .then(rows => {
+      result(null, rows);
+    })
+    .catch(err => {
+      result(null, err);
     });
-  };
+  });
+};
   
-  User.getAll = (result) => {
-    let query = "SELECT * FROM Nutzer";
-  
-    sql.query(query, (err, res) => {
-      if (err) {
-        result(null, err);
-        return;
-      }
-
-      result(null, res);
-    });
-  };
-  
-  User.remove = (id, result) => {
-    sql.query("DELETE FROM Nutzer WHERE id = ?", id, (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-  
-      if (res.affectedRows == 0) {
+User.remove = (id, result) => {
+  sql.then(connection => {
+    connection.query(`DELETE FROM Nutzer WHERE id = ${id}`)
+    .then(rows => {
+      if (rows.length === 0) {
         // not found User with the id
         result({ kind: "not_found" }, null);
         return;
       }
-  
       console.log("deleted user with id: ", id);
-      result(null, res);
+      result(null, rows);
+    })
+    .catch(err => {
+      console.log("error: ", err);
+      result(null, err);
     });
-  };
+  });
+};
   
-  User.removeAll = result => {
-    sql.query("DELETE FROM Nutzer", (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-  
-      console.log(`deleted ${res.affectedRows} users`);
-      result(null, res);
+User.removeAll = result => {
+  sql.then(connection => {
+    connection.query("DELETE FROM Nutzer")
+    .then(rows => {
+      console.log(`deleted ${rows.length} users`);
+      result(null, rows);
+    })
+    .catch(err => {
+      console.log("error: ", err);
+      result(null, err);
     });
-  };
+  });
+};
   
-  module.exports = User;
+module.exports = User;
